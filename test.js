ac919a53744243bf3dbeb86552a99101cada14b1
Jacob Ott <jrott57@gmail.com>
2018-03-19 20:57:45 -0500

    UPDATED JS2

--- test.js
+++
@@ -1,7 +0,0 @@
-function initMap() {
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -32.8412, lng: 96.7845},
-      zoom: 8
-    });
-  }
