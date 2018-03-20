03acdb1854fb8cb581dc8a515937bfa9c8b8215a
Misti Gray <misti.mechelle@gmail.com>
2018-03-19 20:36:51 -0500

    removed extra map api code

--- crimeTracker.js
+++ crimeTracker.js
@@ -9,66 +9,13 @@
   };
   firebase.initializeApp(config);
 
-  //Google geolocation API
-//   $.ajax({
-//     url: "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBPv_oeAKVz-UvKJX8HbJfyemZrjwmQJCk",
-//     method: "GET"
-//   })
+  // Google geolocation API
+  // $.ajax({
+  //   url: "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBPv_oeAKVz-UvKJX8HbJfyemZrjwmQJCk",
+  //   method: "POST"
+  // }).then(function(response){
 
-//Map code 1
-// function initMap() {
-//     var location = new google.maps.LatLng(32.7791, 96.8003);
-
-//     var mapCanvas = document.getElementById("googleMap");
-
-//     var mapOptions = {
-//         center: location,
-//         zoom: 16,
-//         panControl: false,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP
-//     }
-//     var map = new google.maps.Map(mapCanvas, mapOptions);
-// }
-
-// google.maps.event.addDomListner(window, 'load', initMap);
-
-// var mapOptions = {
-//                 center: new google.maps.LatLng(28.1823294, -82.352912),
-//                 zoom: 9,
-//                 mapTypeId: google.maps.MapTypeId.HYBRID,
-//                 scrollwheel: false,
-//                 draggable: false,
-//                 panControl: true,
-//                 zoomControl: true,
-//                 mapTypeControl: true,
-//                 scaleControl: true,
-//                 streetViewControl: true,
-//                 overviewMapControl: true,
-//                 rotateControl: true,
-//             };
-// var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-        
-// google.maps.event.addDomListener(window, 'load', initialize);
-
-//second map code
-function initMap(){
-var mapOptions = {
-    center: new google.maps.LatLng(28.1823294, -82.352912),
-    zoom: 9,
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-    scrollwheel: false,
-    draggable: false,
-    panControl: true,
-    zoomControl: true,
-    mapTypeControl: true,
-    scaleControl: true,
-    streetViewControl: true,
-    overviewMapControl: true,
-    rotateControl: true,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-}
-google.maps.event.addDomListener(window, 'load', initMap);
+  // })
 
   //Dallas open data
   $.ajax({
