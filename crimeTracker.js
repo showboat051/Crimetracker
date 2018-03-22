5064480bb04cf686eed07bd3437250b1f884d7f4
Misti Gray <misti.mechelle@gmail.com>
2018-03-21 21:18:13 -0500

    Updated HTML

--- crimeTracker.js
+++ crimeTracker.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
   // Initialize Firebase
   var config = {
@@ -66,42 +66,42 @@ $(document).ready(function(){
       3. The {content} object is a little popup modal onClick(marker). I think we could put the Description and Address of the crimes
       */
         //This will be an array of markers, where we will feed in coords from the Dallas Open Data API
-        var markers = {};
+        // var markers = {};
 
-        addMarker({
-            coords:{lat:38.8403, lng:-97.6114},
-            iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-            //this content will be overwrittten by the if() statement in the addMarker function
-            content:'<h1>test/h1>'
-            });
+        // addMarker({
+        //     coords:{lat:38.8403, lng:-97.6114},
+        //     iconImage:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        //     //this content will be overwrittten by the if() statement in the addMarker function
+        //     content:'<h1>test/h1>'
+        //     });
 
-        function addMarker(carts){
-            var marker = new google.maps.Marker({
-            position:carts.coords,
-            map: map,
-            icon: {
-                path: google.maps.SymbolPath.CIRCLE,
-                scale: 8
-            }
-            });
+        // function addMarker(carts){
+        //     var marker = new google.maps.Marker({
+        //     position:carts.coords,
+        //     map: map,
+        //     icon: {
+        //         path: google.maps.SymbolPath.CIRCLE,
+        //         scale: 8
+        //     }
+        //     });
 
-            //check for custom icon to prevent undefined
-            if(carts.iconImage){
-                marker.setIcon(carts.iconImage);
-            }
+        //     //check for custom icon to prevent undefined
+        //     if(carts.iconImage){
+        //         marker.setIcon(carts.iconImage);
+        //     }
 
-            //check content to prevent undefined
-            if(carts.content){
-                var infoWindow = new google.maps.InfoWindow({
-                    //here, we will have to feed in two fields from the Dallas Open Data API
-                    content:'<h2>Title</h2><br><h3>address</h3>'
-                });
+        //     //check content to prevent undefined
+        //     if(carts.content){
+        //         var infoWindow = new google.maps.InfoWindow({
+        //             //here, we will have to feed in two fields from the Dallas Open Data API
+        //             content:'<h2>Title</h2><br><h3>address</h3>'
+        //         });
 
-                marker.addListener('click', function(){
-                    infoWindow.open(map, marker);
-                });
-            }
-        }
+        //         marker.addListener('click', function(){
+        //             infoWindow.open(map, marker);
+        //         });
+        //     }
+        // }
         //END MAP MARKERS
 
   //Dallas open data
@@ -110,33 +110,33 @@ $(document).ready(function(){
     type: "GET",
     data: {
       "$limit" : 5000,
-      "$$app_token" : "wuP78c3lOV3O8eisU6WoBMfQ8P"
+      "$$app_token" : "wuP78c3lOV3O8eisU6WoBMfQ8"
     },
 }).done(function(data) {
   alert("Retrieved " + data.length + " records from the dataset!");
   console.log(data);
 });
 
-  $(document).on("click", ".submit", function(query){
-      var queryURL = "https://www.dallasopendata.com/api/views/vkty-8qkv/rows.json?accessType=DOWNLOAD"
+//   $(document).on("click", ".submit", function(query){
+//       var queryURL = "https://www.dallasopendata.com/api/views/vkty-8qkv/rows.json?accessType=DOWNLOAD"
       
-      //so we need to iterate through 389 types of crimes and get the 9th index for the description of the crime. they are nested arrays.
-      //I think there is a different API fieldname that will make it easier to categorize than the Description.
-      $.ajax({
-          url: queryURL,
-          //url not assigned yet
-          method: "GET"
+//       //so we need to iterate through 389 types of crimes and get the 9th index for the description of the crime. they are nested arrays.
+//       //I think there is a different API fieldname that will make it easier to categorize than the Description.
+//       $.ajax({
+//           url: queryURL,
+//           //url not assigned yet
+//           method: "GET"
 
-      }).then(function(response){
-          var dataObject = response.data;
-          // var data;
-          for(i = 0; i < dataObject.length; i++){
-              var crimeResult = response.data[i]
-              console.log(crimeResult[8]);
-              // console.log(response);
-          };
-      });
-  }
-)}
-);
+//       }).then(function(response){
+//           var dataObject = response.data;
+//           // var data;
+//           for(i = 0; i < dataObject.length; i++){
+//               var crimeResult = response.data[i]
+//               console.log(crimeResult[8]);
+//               // console.log(response);
+//           };
+//       });
+//   }
+// )}
+// );
 
